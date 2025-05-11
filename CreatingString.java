7963af6f49c855f824691372784b9cef8f003222
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2025-05-11 09:37:56 +0530

    Add files via upload

---
+++ CreatingString.java
@@ -0,0 +1,55 @@
+//Creating string from character object
+//class CreatingString
+//{
+//    public static void main(String args[])
+//    {
+//        char c[]={'a','b','c','d'};
+//        String s=new String(c);
+//        System.out.println("String:"+s);
+//    }
+//}
+
+//Creating string from particular characters in array
+//class CreatingString
+//{
+//    public static void main(String args[])
+//    {
+//        char c[]={'a','b','c','d','e','f'};
+//        String s=new String(c,1,c.length-1);
+//        System.out.println("String:"+s);
+//    }
+//}
+
+//Creating string using byte array
+//class CreatingString
+//{
+//    public static void main(String args[])
+//    {
+//        byte b[]={66,67,68,69,70};
+//        String s=new String(b);
+//        System.out.println("String:"+s);
+//    }
+//}
+
+//Creating string using particular position of byte array
+//class CreatingString
+//{
+//    public static void main(String args[])
+//    {
+//        byte b[]={66,67,68,69,70};
+//        String s=new String(b,2,3);
+//        System.out.println("String:"+s);
+//    }
+//}
+
+//Creating string from another string object
+//class CreatingString
+//{
+//    public static void main(String args[])
+//    {
+//        byte b[]={66,67,68,69,70};
+//        String s1=new String(b,1,4);
+//        String s2=new String(s1);
+//        System.out.println("String:"+s2);
+//    }
+//}
